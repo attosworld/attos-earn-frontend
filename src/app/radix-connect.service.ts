f3d78da5a2c114bfb6732ec71b37879d74bae6a9
beemdvp <ibrahimpmahmood@gmail.com>
2025-08-27 22:04:05 +0100

    feat: tweak mobile view

--- src/app/radix-connect.service.ts
+++ src/app/radix-connect.service.ts
@@ -14,6 +14,7 @@ import {
   map,
   Observable,
   of,
+  share,
   Subject,
   switchMap,
   tap,
@@ -229,7 +230,8 @@ export class RadixConnectService {
             [data.account]: data as Balances,
           });
         }
-      })
+      }),
+      share()
     );
   }
 
